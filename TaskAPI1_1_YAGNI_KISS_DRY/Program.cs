72ba89a3c7f71fc8645e817f73712beb4da6b54f
Feanor806 <91660671+Feanor806@users.noreply.github.com>
2025-09-24 20:34:31 +0300

    Добавьте файлы проекта.

---
+++ TaskAPI1_1_YAGNI_KISS_DRY/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskAPI1_1_YAGNI_KISS_DRY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
