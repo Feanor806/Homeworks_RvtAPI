9c18e70223ea54314df34dd1585da6c6d8fbd89c
Feanor806 <91660671+Feanor806@users.noreply.github.com>
2025-10-07 20:11:14 +0300

    начало выполнения блока 2

---
+++ TaskAPI2_1_SOLID/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskAPI2_1_SOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
