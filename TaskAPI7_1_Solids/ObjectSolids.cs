083a720f2a4bbfb97e49f92a1131c2ff494b018d
Михаил Солнцев <mihsoln@live.com>
2026-01-10 21:57:49 +0300

    Выполнение блока 7

---
+++ TaskAPI7_1_Solids/ObjectSolids.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAPI7_1_Solids
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ObjectSolids
+    {
+
+    }
+
+    public class NotFamilyInstanceFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return !(elem is FamilyInstance);
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+
+
+}
