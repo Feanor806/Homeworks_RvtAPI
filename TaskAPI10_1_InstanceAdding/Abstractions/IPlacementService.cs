c6892e0f1b505fd89faa9ea6524814946884381e
Михаил Солнцев <mihsoln@live.com>
2026-01-24 22:24:08 +0300

    Выполнение блока 10

---
+++ TaskAPI10_1_InstanceAdding/Abstractions/IPlacementService.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using TaskAPI10_1_InstanceAdding.Models;
+
+namespace TaskAPI10_1_InstanceAdding.Abstractions
+{
+    public interface IPlacementService
+    {
+        Result Place(FurnitureType selectedFurnitureType, int count);
+    }
+}
---
+++ TaskAPI10_1_InstanceAdding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using TaskAPI10_1_InstanceAdding.Abstractions;
+using TaskAPI10_1_InstanceAdding.Models;
+using Autodesk.Revit.UI;
+
+namespace TaskAPI10_1_InstanceAdding.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private FurnitureType _seletedFurnitureType;
+        private int _count;
+        private string _statusMessage;
+        private readonly IPlacementService _placementService;
+
+        public MainWindowViewModel(IPlacementService placementService)
+        {
+            PlaceCommand = new RelayCommand(PlaceFurniture);
+            FurnitireTypes = new ObservableCollection<FurnitureType> 
+            {
+                FurnitureType.Table,
+                FurnitureType.Chair,
+                FurnitureType.Cabinet
+            };
+            _placementService = placementService;
+        }
+
+        private void PlaceFurniture()
+        {
+           CSharpFunctionalExtensions.Result result = _placementService.Place(SelectedFurnitureType, Count);
+            if (result.IsSuccess)
+            {
+                StatusMessage = $"Размещено {Count} единиц мебели.";
+                TaskDialog.Show("Размещение мебели", StatusMessage);
+            }
+            else
+            {
+                StatusMessage = $"Ошибка {result.Error}";
+                TaskDialog.Show("Размещение мебели", StatusMessage);
+            }
+        }
+
+        public ObservableCollection<FurnitureType> FurnitireTypes { get; }
+
+        public FurnitureType SelectedFurnitureType 
+        { 
+            get => _seletedFurnitureType; 
+            set => SetProperty(ref _seletedFurnitureType, value); 
+        }
+
+        public int Count 
+        { 
+            get => _count; 
+            set => SetProperty(ref _count, value); 
+        }
+
+        public string StatusMessage 
+        {
+            get => _statusMessage;
+            set => SetProperty(ref _statusMessage, value);
+        }
+
+        public RelayCommand PlaceCommand { get; }
+    }
+}
